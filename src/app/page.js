80b66d7fbe05ae3d4705a4c4a05397230ab91ff0
papabn <pndiaye999@gmail.com>
2023-04-21 23:30:10 +0000

    add throw new Error ("Failed to fetch data"); i

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default async function Home({searchParams}) {
   const res= await fetch (`https://api.themoviedb.org/3/${genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"}?api_key=${API_KEY}&language=en-US&page=1`,{next: {revalidate: 10000}});
 
   if(!res.ok){
-     alert('retry !!! again something went wrong');
+     throw new Error ("Failed to fetch data");
   }
   const data = await res.json();
   const results = data.results;
