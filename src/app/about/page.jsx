816983d6cddd7bcea06b89006705d47d78207cd6
papabn <pndiaye999@gmail.com>
2023-04-21 23:02:36 +0000

    remove something in about/page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -5,7 +5,7 @@ export default function page() {
     <div className="max-w-6xl mx-auto space-y-4 p-4">
       <h1 className="text-2xl font-medium text-amber-600 ">About</h1>
       <p>
-        IMDb (an acronym for Internet Movie Database) is an online database of
+        IMDb an acronym for Internet Movie Database is an online database of
         information related to films, television series, podcasts, home videos,
         video games, and streaming content online-including cast, production
         crew and personal biographies, plot summaries, trivia, ratings, and fan
@@ -15,7 +15,7 @@ export default function page() {
       <p>
         Since 1998, it is now owned and operated by IMDb.com, Inc., a subsidiary
         of Amazon. As of March 2022, the database contained some 10.1 million
-        titles (including television episodes) and 11.5 million person records.
+        titles including television episodes and 11.5 million person records.
         Additionally, the site had 83 million registered users. The site's
         message boards were disabled in February 2017. As of 2019, IMDb is the
         52th most visited website in the world.
