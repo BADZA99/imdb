025b7da425cff5acd53065124f377e5359329112
papabn <pndiaye999@gmail.com>
2023-04-21 23:16:51 +0000

    remove ' " in about/page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,23 +1,22 @@
 import React from "react";
-
 export default function page() {
   return (
     <div className="max-w-6xl mx-auto space-y-4 p-4">
       <h1 className="text-2xl font-medium text-amber-600 ">About</h1>
       <p>
         IMDb an acronym for Internet Movie Database is an online database of
-        information related to films, television series, podcasts, home videos,
-        video games, and streaming content online-including cast, production
-        crew and personal biographies, plot summaries, trivia, ratings, and fan
+        information related to films television series podcasts home videos
+        video games and streaming content online-including cast production
+        crew and personal biographies plot summaries trivia ratings and fan
         and critical reviews. IMDb began as a fan-operated movie database on the
-        Usenet group "rec.arts.movies" in 1990, and moved to the Web in 1993.
+        Usenet group rec.arts.movies in 1990 and moved to the Web in 1993.
       </p>
       <p>
-        Since 1998, it is now owned and operated by IMDb.com, Inc., a subsidiary
-        of Amazon. As of March 2022, the database contained some 10.1 million
+        Since 1998 it is now owned and operated by IMDb.com Inc a subsidiary
+        of Amazon. As of March 2022 the database contained some 101 million
         titles including television episodes and 11.5 million person records.
-        Additionally, the site had 83 million registered users. The site's
-        message boards were disabled in February 2017. As of 2019, IMDb is the
+        Additionally the site had 83 million registered users. The sites
+        message boards were disabled in February 2017.As of 2019 IMDb is the
         52th most visited website in the world.
       </p>
     </div>
